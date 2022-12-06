21ff7d07a225540e9410fd0cef021caad109043d
Sassus Capelo <synedcap@gmail.com>
2022-12-06 14:06:20 +0100

    crud user and login

--- routes/auth.js
+++ routes/auth.js
@@ -1,26 +1,56 @@
 var express = require('express');
 var router = express.Router();
+var bcrypt = require('bcrypt');
 const User = require("../models/user");
 
-/* REGISTER */
+/* Register */
 router.post('/register',async (req, res) => {
 
-
+    
     try {
-        
+        const salt = await bcrypt.genSalt(10);
+        //save user and send status
         const user = await new User({
             username: req.body.username,
             email:req.body.email,
-            password: req.body.password
+            password: await bcrypt.hash(req.body.password, salt)
         })
     
         await user.save();
 
         res.status(200).json(user);
     } catch (err) {
-        console.log(err);
+        res.status(500).json(err);
     }
    
-  });
+});
+
+/* Login */
+router.post('/login',async (req, res) => {
+
+    
+    try {
+         //search user
+        const user = await User.findOne({email : req.body.email });
+
+        if (!user) {
+            res.status(404).json("user not found");
+        }
+        else{
+            //verify that password are the same
+            const password_valid = await bcrypt.compare(req.body.password,user.password);
+      
+            if(password_valid){
+                res.status(200).json(user);
+            }else{
+                res.status(400).json("Wrong password");
+            } 
+        }
+
+    } catch(err){
+        res.status(500).json(err);
+    }
+   
+});
   
   module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,99 @@
 var express = require('express');
 var router = express.Router();
+var bcrypt = require('bcrypt');
+const User = require("../models/user");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+/* update user */
+router.put('/:id',async (req, res) => {
+
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+
+    if (req.body.password) {
+        try{
+          const salt = await bcrypt.genSalt(10);
+          req.body.password = await bcrypt.hash(req.body.password, salt);
+
+        }catch(err){
+          return res.status(500).json(err);
+
+        }
+    }
+
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id,{
+        $set: req.body,
+      })
+      res.status(200).json("Account has been updated");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+
+  }else{
+    return res.status(403).json("You can update only account!")
+  }
+});
+/* delete user */
+router.delete('/:id',async (req, res) => {
+
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+
+    try {
+      const user = await User.findByIdAndDelete(req.params.id)
+      res.status(200).json("Account has been deleted");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+
+  }else{
+    return res.status(403).json("You can delete only account!")
+  }
+});
+/* get a user */
+router.get('/:id',async (req, res) => {
+
+    try {
+      const user = await User.findById(req.params.id)
+
+      const {password,updatedAt, ...other} = user._doc;
+      res.status(200).json(other);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+
+});
+/* follow user */
+router.put('/:id/follow',async (req, res) => {
+
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentuser = await User.findById(req.body.userId);
+
+      if(!user.followers.includes(req.body.userId)){
+
+          await user.updateOne({$push: {followers: req.body.userId}})
+          await currentuser.updateOne({$push: {followings: req.params.id}})
+          res.status(200).json(" user has been followed")
+      }else{
+        res.status(403).json("you already follow this user")
+      }
+      
+    } catch (err) {
+      
+    }
+    
+  }else{
+    res.status(403).json("you cant follow yourself")
+  }
+
+});
+
+/* unfollow user */
+
 
 module.exports = router;

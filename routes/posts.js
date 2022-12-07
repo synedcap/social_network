7e6fd4073077ddec3cda6e3e56517b9cdfd794bd
Sassus Capelo <synedcap@gmail.com>
2022-12-07 11:12:31 +0100

    route to like or dislike a post,route to get a post,route to get timeline post

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const Post = require("../models/Post");
+const User = require("../models/user");
 
 /* create post */  
 router.post('/', async (req, res) => {
@@ -59,6 +60,60 @@ router.delete('/:id', async (req, res) => {
 
     } catch (err) {
         res.status(500).json(err);
+    }
+
+});
+
+/* like or dislike post */  
+router.put('/:id/like', async (req, res) => {
+
+    try {
+
+        const post = await Post.findById(req.params.id);
+        
+        if(!post.likes.includes(req.body.userId)){
+
+            await post.updateOne({ $push:{ likes:req.body.userId }});
+            res.status(200).json("post has been liked");
+
+        }else{
+            await post.updateOne({ $pull:{ likes:req.body.userId }});
+            res.status(200).json("post has been disliked");
+        }
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+
+});
+
+/*get a post */
+router.get('/:id',async (req, res) => {
+
+    try {
+      const post = await Post.findById(req.params.id)
+
+      res.status(200).json(post);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+
+});
+/*get timeline post */
+router.get('/timeline/all',async (req, res) => {
+
+    try {
+      const currentUser = await User.findById(req.body.userId);
+      const userPosts = await Post.find({ userId : currentUser._id});
+      const friendPosts = await Promise.all(
+        currentUser.followings.map((friendId) => {
+           return Post.find({ userId : friendId})
+        })
+      );
+
+      res.json(userPosts.concat(...friendPosts));
+    } catch (err) {
+      res.status(500).json(err);
     }
 
 });
